f1effc1f04d3a90ea3fa4116fc3692d9afaf05ad
MUHAMMMMED <amer66965@gmail.com>
2024-09-12 11:53:05 +0300

    Upload  email  code

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -184,10 +184,7 @@ EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_PORT = 587   
  
- 
-
-
-
+  
 # Celery Configuration
 CELERY_BROKER_URL = 'redis://redis:6379/0'
 CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
--- frontend/src/pages/Dashboard/pages/EmailMarketing/components/Customer/Customer.jsx
+++ frontend/src/pages/Dashboard/pages/EmailMarketing/components/Customer/Customer.jsx
@@ -1,32 +1,48 @@
 
-import React, { useState } from 'react';
+
+import React, { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../../../../../../desing-system/Authentication/UserProvider';
 import ErrorPage from '../../../../../../desing-system/components/Loading/ErrorPage';
 import Sidebar_dashboard from '../../../../components/Sidebar_dashboard/Sidebar_dashboard';
 import StudentsList from './StudentsList/StudentsList';
 
 const Customer = () => {
-  const { userData } = UserContext(UserContext); // Use useContext to get userData
+  const { userData } = useContext(UserContext); // Use useContext correctly
   const [showSidebar, setShowSidebar] = useState(false);
 
   const toggleSidebar = () => {
     setShowSidebar(!showSidebar);
   };
 
+  // useEffect to handle page reload logic
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const hasRefreshed = localStorage.getItem('hasRefreshed');
+        if (!hasRefreshed) {
+          localStorage.setItem('hasRefreshed', 'true');
+          window.location.reload(); // Reload the page if userData is not available
+          return;
+        }
+        localStorage.removeItem('hasRefreshed');
+      } catch (error) {
+        console.error('Error fetching data:', error); // Log any errors during fetching
+      }
+    };
 
+    fetchData();
+  }, []); // Removed the navigate dependency as it's not defined
 
+  // Handle access denied based on user_type
   if (userData && userData?.user_type !== 'M') {
     return <ErrorPage head="Access Denied" error="You do not have the required permissions." />;
   }
 
-
-
   return (
-    <div div className='Dashboard_container' >
+    <div className='Dashboard_container'>
       <Sidebar_dashboard showSidebar={showSidebar} toggleSidebar={toggleSidebar} />
       <StudentsList />
-
-    </div >
+    </div>
   );
 };
 
